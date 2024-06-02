d888e3da53384a968f5da4d64a1236eba6bbc1c2
ayaan khan <ayaan.khan2812@gmail.com>
2024-06-02 21:09:50 +0530

    final1

--- src/App.js
+++ src/App.js
@@ -9,20 +9,6 @@ import Profession from './pro';
 import Last from './last';
 
 
-const items = [
-  {
-    title: 'Section 1',
-    content: 'Content for Section 1'
-  },
-  {
-    title: 'Section 2',
-    content: 'Content for Section 2'
-  },
-  {
-    title: 'Section 3',
-    content: 'Content for Section 3'
-  }
-];
 function App() {
   
   return (
